e224221144915200f8b573692875000a3ee0662b
markus-mark40 <143330970+markus-mark40@users.noreply.github.com>
2026-08-28 21:26:44 +0800

    chore: add initial variables

---
+++ script.js
@@ -0,0 +1,12 @@
+let studentNumbers = 0;
+let studentNumber = 0;
+let studentScore = 0;
+
+let passingCount = 0;
+let failingCount = 0;
+
+let highestScore = 0;
+let lowestScore = 0;
+
+let averageScore = 0;
+let totalScore = 0;
